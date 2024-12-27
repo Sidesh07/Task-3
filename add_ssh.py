6d2a15891c52fdae52437c6b9d49a3ab312544d5
Ubuntu <ubuntu@ip-172-31-31-204.ec2.internal>
2024-12-27 08:54:15 +0000

    initial commit

--- add_ssh.py
+++ add_ssh.py
@@ -19,8 +19,8 @@ def add_ssh_key_to_gitlab(api_url, api_token, ssh_key, title):
         print(f"An error occurred while communicating with GitLab: {e}")
 
 if __name__ == "__main__":
-    gitlab_api_url = url
-    gitlab_api_token = api
-    ssh_key=ssh
-    ssh_key_title = "My SSH Key"
-    add_ssh_key_to_gitlab(gitlab_api_url, gitlab_api_token, ssh_key, ssh_key_title)
+    #gitlab_api_url = url
+    #gitlab_api_token = api
+    #ssh_key=ssh
+    #ssh_key_title = "My SSH Key"
+    #add_ssh_key_to_gitlab(gitlab_api_url, gitlab_api_token, ssh_key, ssh_key_title)
